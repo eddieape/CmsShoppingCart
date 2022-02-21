3b7f68f8437af0d9027c7adac3b7c9c66f580083
Eddie <eddie_ape@hotmail.com>
2022-02-21 16:06:07 +1300

    Create Admin area;

---
+++ CmsShoppingCart/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new CmsShoppingCartContext(serviceProvider.GetRequiredService<DbContextOptions<CmsShoppingCartContext>>()))
+            {
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+
+                context.Pages.AddRange(
+                    new Page
+                    {
+                        Title = "Home",
+                        Slug = "home",
+                        Cotent = "home page",
+                        Sorting = 0
+                    },
+                    new Page
+                    {
+                        Title = "About",
+                        Slug = "about-us",
+                        Cotent = "about us page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Services",
+                        Slug = "servcies",
+                        Cotent = "servcies page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Context",
+                        Slug = "contact",
+                        Cotent = "contact page",
+                        Sorting = 100
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- CmsShoppingCart/Program.cs
+++ CmsShoppingCart/Program.cs
@@ -1,5 +1,7 @@
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,22 @@ namespace CmsShoppingCart
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var servcies = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(servcies);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
