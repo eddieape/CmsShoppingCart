b8115b9e78d28d55f50f36474653a9bff035f7eb
Eddie <eddie_ape@hotmail.com>
2022-02-22 15:12:43 +1300

    Add validation to the page model;

--- CmsShoppingCart/Models/Page.cs
+++ CmsShoppingCart/Models/Page.cs
@@ -9,12 +9,12 @@ namespace CmsShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required]
-        [Display(Name = "Fruit")]
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        //[Display(Name = "Fruit")]
         public string Title { get; set; }
-        [Required]
+
         public string Slug { get; set; }
-        [Required]
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 4")]
         public string Cotent { get; set; }
 
         public int Sorting { get; set; }
