619b84ebdc57b9cbb6a0deb4ecbcb99365f89af2
Eddie <eddie_ape@hotmail.com>
2022-03-13 13:58:39 +1300

    Create the present page of products;

---
+++ CmsShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            _context = context;
+
+        }
+
+        //GET /admin/products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+            var products = _context.Products.OrderByDescending(x => x.Id)
+                                            .Skip((p - 1) * pageSize)
+                                            .Take(pageSize);
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Products.Count() / pageSize);
+
+            return View(await products.ToListAsync());
+        }
+    }
+}
