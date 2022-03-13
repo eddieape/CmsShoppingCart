ec3d50a55ca30163457c75729ca773ff8369b271
Eddie <eddie_ape@hotmail.com>
2022-03-13 16:18:02 +1300

    Create cart model;

---
+++ CmsShoppingCart/Models/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal GrandTotal { get; set; }
+
+
+    }
+}
