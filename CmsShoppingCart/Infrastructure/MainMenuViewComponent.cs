152ed4c1b653795ca3888f8575b9b0edc3664081
Eddie <eddie_ape@hotmail.com>
2022-03-13 13:44:53 +1300

    Create main menu component;

---
+++ CmsShoppingCart/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly CmsShoppingCartContext _context;
+
+        public MainMenuViewComponent(CmsShoppingCartContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPageAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPageAsync()
+        {
+            return _context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
